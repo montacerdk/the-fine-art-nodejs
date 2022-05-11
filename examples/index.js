e06b38c6d1e8519622e0151ada51b5776e3f08b0
Montacer Dkhilali <montacer.dkhilali@unicornpowered.com>
2022-05-11 12:05:21 -0400

    Add routes to server

--- examples/index.js
+++ examples/index.js
@@ -4,6 +4,30 @@ const SERVER_PORT = 3002;
 
 const server = http.createServer((req, res) => {
   console.log(`Server is listening on port ${SERVER_PORT}`);
+  // console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+
+  if (req.url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>My home page</title></head>");
+    res.write(
+      "<body style='font-size:70px;'>Hello from our Node.js Server Home page</body>"
+    );
+    res.write("</html>");
+    res.statusCode = 200;
+    return res.end();
+  }
+
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write(
+    "<body style='font-size:70px;'>Hello from our Node.js Server</body>"
+  );
+  res.write("</html>");
+  res.statusCode = 200;
+  res.end();
 });
 
 server.listen(SERVER_PORT);
